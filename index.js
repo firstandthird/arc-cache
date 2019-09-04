d215e6a8cde73781cb3742e0a8c3d9d0519db771
orthagonal <orthagonal1@gmail.com>
2019-09-04 16:47:31 +0000

    don't alter request

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const cache = require('@firstandthird/memory-cache');
 
 // defaults to 10 minutes:
 const cacheReply = function(req, fn, replyCacheTTL = 60000 * 10) {
-  if (req.queryStringParameters) {
-    req.query = req.queryStringParameters;
-  }
+  const key = req.queryStringParameters ? 'queryStringParameters' : 'query';
   return cache.memo(`response-${req.path}`, () => {
     return fn(req);
-  }, replyCacheTTL, (req.query.update === '1'));
+  }, replyCacheTTL, (req[key].update === '1'));
 };
 
 module.exports = { cacheReply, cache };
