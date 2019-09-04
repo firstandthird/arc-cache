d83477848ad8bcc461ae28d3811ba89a30131469
orthagonal <orthagonal1@gmail.com>
2019-09-04 16:55:50 +0000

    comment explaining

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cache = require('@firstandthird/memory-cache');
 
 // defaults to 10 minutes:
 const cacheReply = function(req, fn, replyCacheTTL = 60000 * 10) {
+  // architect version 6 uses different keys:
   const key = req.queryStringParameters ? 'queryStringParameters' : 'query';
   return cache.memo(`response-${req.path}`, () => {
     return fn(req);
