2954a3a0004b47bb104da39a758a9fa51cfeb3f0
orthagonal <orthagonal1@gmail.com>
2019-09-17 23:11:17 +0000

    use function-cache and return as a handler

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const cache = require('@firstandthird/memory-cache');
+// const cache = require('@firstandthird/memory-cache');
+const FunctionCache = require('@firstandthird/function-cache');
+const cache = new FunctionCache(false);
 
 // defaults to 10 minutes:
-const cacheReply = function(req, fn, replyCacheTTL = 60000 * 10) {
+// const cacheReply = function(req, fn, replyCacheTTL = 60000 * 10) {
+//   // architect version 6 uses different keys:
+//   const key = req.queryStringParameters ? 'queryStringParameters' : 'query';
+//   return cache.memo(`response-${req.path}`, () => {
+//     return fn(req);
+//   }, replyCacheTTL, (req[key].update === '1'));
+// };
+
+const cacheReply = function(fn, cacheOptions = {}) {
   // architect version 6 uses different keys:
-  const key = req.queryStringParameters ? 'queryStringParameters' : 'query';
-  return cache.memo(`response-${req.path}`, () => {
-    return fn(req);
-  }, replyCacheTTL, (req[key].update === '1'));
+  const ttl = cacheOptions.ttl || (60000 * 10);
+  const dropQueryParam = cacheOptions.dropQueryParam || 'update';
+  const skipQueryParam = cacheOptions.skipQueryParam || 'skip';
+  const statsQueryParam = cacheOptions.statsQueryParam || 'stats';
+  const keyMethod = cacheOptions.key || (req => {
+    const queryKey = req.queryStringParameters ? 'queryStringParameters' : 'query';
+    const query = req[queryKey];
+    return cacheOptions.cacheQueryParams ? `response-${req.path}-${JSON.stringify(query)}`: `response-${req.path}`;
+  });
+  return async function(req) {
+    const queryKey = req.queryStringParameters ? 'queryStringParameters' : 'query';
+    const query = req[queryKey];
+    const memoKey = keyMethod(req);
+    // return cache stats if requested:
+    if (query[statsQueryParam]) {
+      return cache.cache.getStats();
+    }
+    // skip cache altogether if requested:
+    if (query[skipQueryParam]) {
+      return fn(req);
+    }
+    // return the cached value:
+    return cache.memo(
+      memoKey,
+      () => fn(req),
+      ttl,
+      query[dropQueryParam] // force-update cache if true
+    );
+  };
 };
 
 module.exports = { cacheReply, cache };
--- test/test.arc6.js
+++ test/test.arc6.js
@@ -27,38 +27,3 @@ tap.test('cache arc6', async t => {
   t.equal(count, 2, 'new method executes, does not conflict with previous cached method');
   t.end();
 });
-
-tap.test('cacheReply arc6', async t => {
-  let count = 0;
-  // declare a request handler:
-  const handler = function(request) {
-    count++;
-    return request.path;
-  };
-  const request = {
-    path: 'yes',
-    queryStringParameters: {
-      all: 'yes'
-    }
-  };
-  const r1 = await arcCache.cacheReply(request, handler);
-  t.match(r1, 'yes', 'handler returns value');
-  t.equal(count, 1, 'handler executed first time');
-
-  const r2 = await arcCache.cacheReply(request, handler);
-  t.match(r2, 'yes', 'cached method returns value');
-  t.equal(count, 1, 'cached method not executed');
-  request.queryStringParameters.all = 'no';
-
-  request.path = 'no';
-  const r3 = await arcCache.cacheReply(request, handler);
-  t.match(r3, 'no', 'new method returns value');
-  t.equal(count, 2, 'new method executes, does not conflict with previous cached method');
-
-  request.path = 'yes';
-  request.queryStringParameters.update = '1';
-  const r4 = await arcCache.cacheReply(request, handler);
-  t.match(r4, 'yes', 'handler returns value');
-  t.equal(count, 3, 'handler executed first time');
-  t.end();
-});
